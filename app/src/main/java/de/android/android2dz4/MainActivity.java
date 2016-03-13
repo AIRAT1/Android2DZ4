4d98d3bc40a65ab1a1b69413a81aa167bb6eb4e2
Ayrat <ayrat1@mail.ru>
2016-03-13 10:00:31 +0300

    create AsyncTasc network connection

--- app/src/main/java/de/android/android2dz4/MainActivity.java
+++ app/src/main/java/de/android/android2dz4/MainActivity.java
@@ -1,24 +1,41 @@
 package de.android.android2dz4;
 
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
     private final String LOG = "LOG";
     private Button btnThread;
     private Handler handler;
+    private TextView textView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnThread = (Button)findViewById(R.id.btnThread);
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
         handler = new Handler(new Handler.Callback() {
             @Override
             public boolean handleMessage(Message msg) {
@@ -30,7 +47,7 @@ public class MainActivity extends Activity {
             }
         });
     }
-
+    // create new thread
     public void onClickThread(View view) {
         btnThread.setEnabled(false);
         new Thread(new Runnable() {
@@ -47,8 +64,17 @@ public class MainActivity extends Activity {
             }
         }).start();
     }
-
+    // network connection with asyncTask
     public void onClickAsync(View view) {
+        String stringUrl = editText.getText().toString();
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        // проверяем наличие соединения с инетом
+        if (ni != null && ni.isConnected()) {
+            new NetTask().execute(stringUrl);
+        }else {
+            Toast.makeText(MainActivity.this, "No network connection available.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onClickService(View view) {
@@ -57,4 +83,57 @@ public class MainActivity extends Activity {
     public void onClickDownload(View view) {
     }
 
+
+
+
+
+
+    private class NetTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            }catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        private String downloadUrl(String myUrl) throws IOException{
+            InputStream is = null;
+            // количество выводимых символов
+            int len = 1000;
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(15000);
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+                int response = connection.getResponseCode();
+                Log.d(LOG, "The response is: " + response);
+                is = connection.getInputStream();
+                // формируем строку вывода
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            }finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        private String readIt(InputStream is, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(is, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            textView.setText(s);
+        }
+    }
 }
