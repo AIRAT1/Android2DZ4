c86c4d265eb94b6c9dd2760fbed168f7f14137ab
Ayrat <ayrat1@mail.ru>
2016-03-13 09:14:12 +0300

    create new Thread function with handler

--- app/src/main/java/de/android/android2dz4/MainActivity.java
+++ app/src/main/java/de/android/android2dz4/MainActivity.java
@@ -1,13 +1,60 @@
 package de.android.android2dz4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
 
+public class MainActivity extends Activity {
+    private final String LOG = "LOG";
+    private Button btnThread;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnThread = (Button)findViewById(R.id.btnThread);
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if (msg.what == 100500) {
+                    btnThread.setEnabled(true);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
+    public void onClickThread(View view) {
+        btnThread.setEnabled(false);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(LOG, "start");
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d(LOG, "stop");
+                handler.sendEmptyMessage(100500);
+            }
+        }).start();
+    }
+
+    public void onClickAsync(View view) {
+    }
+
+    public void onClickService(View view) {
+    }
+
+    public void onClickDownload(View view) {
+    }
+
 }
